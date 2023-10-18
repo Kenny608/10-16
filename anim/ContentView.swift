e9744cfadeb64a91336700ac0af4effbf4456cd2
Kenny <kennypluchau@gmail.com>
2023-10-17 20:24:29 -0700

    added files

---
+++ anim/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  anim
+//
+//  Created by Kenny Luchau on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ anim/animApp.swift
@@ -0,0 +1,17 @@
+//
+//  animApp.swift
+//  anim
+//
+//  Created by Kenny Luchau on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct animApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
